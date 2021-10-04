4cb343734030de0679412733c03d1b0e8ddb36df
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2021-10-04 18:55:17 -0300

    Correção em testes

--- tests/testthat/test-get_budgetary_exp_by_gdp_range.R
+++ tests/testthat/test-get_budgetary_exp_by_gdp_range.R
@@ -1,4 +1,8 @@
 test_that("Get government budget expenses for municipalities on a rank of gdp", {
-  expect_gt(NROW(get_budgetary_exp_by_gdp_range(gdp_range=c(1,100), gdp_year=2018,exp_year = 2019, account = "Pessoal e Encargos Sociais" )),0)
+
+  projeto <- read_csv("~/Github/siconfiBD/projeto.csv")
+
+
+  setup_siconfi(projeto$projeto)
   expect_gt(NROW(get_budgetary_exp_by_gdp_range(gdp_range=c(1,100), gdp_year=2018,exp_year = 2019, account = "Pessoal e Encargos Sociais", expense_stage= "Despesas Empenhadas" )),0)
 })
